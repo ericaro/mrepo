eb136bde41e458658d376971bfa9c29418edc757
Eric <eric@ericaro.net>
2015-03-16 18:25:32 +0100

    fixed issue with sbr ci dashboard
    beeing in a sbr workspace should not be required

--- cmd/dashboardCmd.go
+++ cmd/dashboardCmd.go
@@ -26,11 +26,11 @@ func (c *DashboardCmd) Flags(fs *flag.FlagSet) {
 }
 
 func (c *DashboardCmd) Run(args []string) {
-	wd := FindRootCmd()
 
 	//use the server from git config if none were specified
 	if *c.server == "" { // no one specified use the one in config get
 		var err error
+		wd := FindRootCmd()
 		*c.server, err = git.ConfigGet(wd, "ci.server")
 		if err != nil {
 			fmt.Printf("Error, cannot read ci remote address in git config. %s\n Use \ngit config --add ci.server <ci address>\n", err.Error())
